14b3d69d46a8ac1a1d92954d1aba9d5447722f71
gh0c <goran.hrzenjak@bornfight.com>
2021-11-18 21:29:29 +0100

    Add fixtures class generator

---
+++ src/Resources/skeleton/infrastructure/Fixtures.tpl.php
@@ -0,0 +1,62 @@
+<?= "<?php\n" ?>
+
+declare(strict_types=1);
+
+namespace <?= $namespace ?>;
+
+use <?= $entity_full_class_name ?> as <?= $entity_alias ?>;
+use App\Shared\Application\Factory\UuidGeneratorInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class <?= $class_name ?> extends Fixture implements DependentFixtureInterface
+{
+    private UuidGeneratorInterface $uuidGenerator;
+
+    public function __construct(UuidGeneratorInterface $uuidGenerator)
+    {
+        $this->uuidGenerator = $uuidGenerator;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach ($this->getData() as $<?= $object_name ?>Data) {
+            $id = $<?= $object_name ?>Data['id'] ?? $this->uuidGenerator->generate();
+            $<?= $object_name ?> = new <?= $entity_alias ?>(
+                $id,
+<?php foreach ($field_names as $idx => $field): ?>
+                $<?= $object_name ?>Data['<?= $field ?>']<?= $idx < $fields_count - 1 ? ",\n" : "\n" ?>
+<?php endforeach; ?>
+            );
+
+            $manager->persist($<?= $object_name ?>);
+
+            $this->setReference($id, $<?= $object_name ?>);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return array<int, array<string, mixed>>
+     */
+    private function getData(): array
+    {
+        return [
+            [
+<?php foreach ($field_names as $idx => $field): ?>
+                '<?= $field ?>' => <?= $field_fixture_values[$idx] ?>,
+<?php endforeach; ?>
+            ],
+        ]
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDependencies(): array
+    {
+        return [];
+    }
+}
---
+++ src/Services/ClassGenerator/Infrastructure/FixturesClassGenerator.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bornfight\MabooMakerBundle\Services\ClassGenerator\Infrastructure;
+
+use Bornfight\MabooMakerBundle\Services\ClassManipulator\EntityField;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\Str;
+use Symfony\Bundle\MakerBundle\Util\ClassNameDetails;
+
+class FixturesClassGenerator
+{
+    private Generator $generator;
+
+    public function __construct(Generator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * @param EntityField[] $fields
+     */
+    public function generateMutationClass(
+        ClassNameDetails $fixturesClassDetails,
+        ClassNameDetails $entityClassDetails,
+        ClassNameDetails $domainModelClassDetails,
+        array $fields
+    ): string {
+
+        $fieldNames = [];
+        $fieldFixtureValues = [];
+
+        foreach ($fields as $field) {
+            if ('id' === $field->name) {
+                continue;
+            }
+
+            $fieldNames[] = $field->name;
+
+            switch (true) {
+                case 'int' === $field->typeHint:
+                    $fieldFixtureValues[] = 10;
+                    break;
+                case 'string' === $field->typeHint:
+                    $fieldFixtureValues[] = 'example';
+                    break;
+                case 'float' === $field->typeHint:
+                    $fieldFixtureValues[] = 20.00;
+                    break;
+                case 'bool' === $field->typeHint:
+                    $fieldFixtureValues[] = true;
+                    break;
+                default:
+                    $fieldFixtureValues[] = 'Undefined';
+            }
+        }
+
+        return $this->generator->generateClass(
+            $fixturesClassDetails->getFullName(),
+            __DIR__ . '/../../../Resources/skeleton/infrastructure/Fixtures.tpl.php',
+            [
+                'domain_model' => $domainModelClassDetails->getShortName(),
+                'object_name' => Str::asLowerCamelCase($domainModelClassDetails->getShortName()),
+                'entity_full_class_name' => $entityClassDetails->getFullName(),
+                'entity_alias' => 'Doctrine' . $entityClassDetails->getShortName(),
+                'fields_count' => count($fields),
+                'field_names' => $fieldNames,
+                'field_fixture_values' => $fieldFixtureValues,
+            ]
+        );
+    }
+}
